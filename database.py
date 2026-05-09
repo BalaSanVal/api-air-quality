0406b1c86fd1bdb751efcfd4fad4f48c379b7745
Balam Sánchez Valdivia <152461925+BalaSanVal@users.noreply.github.com>
2026-05-08 19:10:26 -0600

    Update database.py with SSL

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,10 +11,19 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL no está configurada en las variables de entorno")
 
+ssl_context = ssl.create_default_context()
+
+# Para prueba inicial con RDS:
+# cifra la conexión, pero no exige validación estricta del certificado.
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={"ssl": {}}
+    connect_args={
+        "ssl": ssl_context
+    }
 )
 
 SessionLocal = sessionmaker(
